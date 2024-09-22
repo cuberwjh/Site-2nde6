42f21543d86edd65babb48dd7898bf254ef31536
cuberwjh <wang.lucas2009@gmail.com>
2024-09-22 12:47:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                        // Définir les constantes :
+
+// Modules :
+//const fs = require('fs');
+
+// Références :
+const form = document.getElementById("ajouter-un-devoir");
+const texte = document.getElementById("new-task");
+const date = document.getElementById("new-task-date");
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                        // Définir les fonctions :
+
+function envoi_data(event) {
+    tache = date.value + " - " + texte.value
+
+    
+}
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                        // Programme main :
+
+form.addEventListener("submit", envoi_data);
+                                        
+                                        //Récupérer la valeur
