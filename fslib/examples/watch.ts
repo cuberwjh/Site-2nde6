bfa936acbd0a5fc96b8d75419e84a21f11a1b64a
cuberwjh <wang.lucas2009@gmail.com>
2024-09-29 00:18:49 +0200

    Delete fslib directory

--- fslib/examples/watch.ts
+++
@@ -1,9 +0,0 @@
-import * as libfs from '../fs';
-import * as fs from 'fs';
-
-
-var dir = __dirname + '/dir';
-// fs.watch(dir, {}, function(event, filename) {
-libfs.watch(dir, {}, function(event, filename) {
-    console.log('event', event, filename);
-});
